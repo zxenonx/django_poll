f3ec77c833044f5e31910fc930c9fdbee5582e3c
xzenon <forlanfr_8@hotmail.fr>
2022-03-18 13:22:19 +0100

    Add form

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 
-from polls.models import Question
+from polls.models import Choice, Question
 
 # Create your views here.
 
@@ -21,8 +23,23 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f"Results for {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request, question_id):
-    return HttpResponse(f"Voting for {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {
+            "question": question,
+            "error_message": "U didn't select a choice"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
